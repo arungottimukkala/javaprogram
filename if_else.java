544c97e569a76894f297731c41058c4454889d10
arun teja <gottimukkalaarun@gmail.com>
2016-05-30 13:49:39 -0500

    all

---
+++ if_else.java
@@ -0,0 +1,17 @@
+package program;
+
+public class if_else {
+	
+public static void main(String args[]){
+
+ int x = 30;
+ if( x < 20 )
+ {
+System.out.print("This is if statement");
+}
+ else {
+
+System.out.print("This is else statement");
+}
+}
+}
---
+++ single.java
@@ -0,0 +1,33 @@
+package program;
+
+public class single {
+	class D
+
+	{
+
+	public void single()
+
+	{
+
+	}
+
+	}
+
+	public  class SingleInherintance extends D
+
+	{
+
+	public void main(String args[])
+
+	{
+
+	System.out.println("sucessfully done");
+
+	SingleInherintance s= new SingleInherintance();
+
+	s.single();
+
+	}
+
+	}
+}
